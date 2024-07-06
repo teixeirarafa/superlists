a56f59ab24d6999d0fa14710b4f41d1c758a3391
Auri Vincenzi <aurimrv@gmail.com>
2024-07-06 09:48:31 -0300

    Model for list Items and associated migrations

--- lists/tests.py
+++ lists/tests.py
@@ -16,3 +16,27 @@ class HomePageTest(TestCase):
 		response = self.client.post('/', data={'item_text': 'A new list item'})
 		self.assertIn('A new list item', response.content.decode())
 		self.assertTemplateUsed(response, 'home.html')
+
+# Inserir abaixo do código já exitente
+
+from lists.models import Item
+
+class ItemModelTest(TestCase):
+
+	def test_saving_and_retriving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEquals(saved_items.count(),2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+
+		self.assertEquals(first_saved_item.text, 'The first (ever) list item')
+		self.assertEquals(second_saved_item.text, 'Item the second')
