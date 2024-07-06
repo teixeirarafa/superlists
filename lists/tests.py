49b8c861fbd55fa7e1775b62bf09c20ad22b57fe
Auri Vincenzi <aurimrv@gmail.com>
2024-07-06 12:05:51 -0300

    Test case updated to check connection between items and lists. Models updated to include List.

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List #
 
 class HomePageTest(TestCase):
 
@@ -28,9 +28,8 @@ class NewListTest(TestCase):
 
 	def test_redirects_after_POST(self):
 		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-		#self.assertEquals(response.status_code, 302)
-		#self.assertEquals(response['location'], '/lists/the-only-list-in-the-world/')
 		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+
 
 class ListViewTest(TestCase):
 
@@ -40,8 +39,9 @@ class ListViewTest(TestCase):
 
 
 	def test_displays_all_list_itens(self):
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
+		list_ = List.objects.create()
+		Item.objects.create(text='itemey 1', list=list_)
+		Item.objects.create(text='itemey 2', list=list_)
 
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -49,16 +49,24 @@ class ListViewTest(TestCase):
 		self.assertContains(response, 'itemey 2')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase): #
 
 	def test_saving_and_retriving_items(self):
+		list_ = List() #
+		list_.save() #
+
 		first_item = Item()
 		first_item.text = 'The first (ever) list item'
+		first_item.list = list_ #
 		first_item.save()
 
 		second_item = Item()
 		second_item.text = 'Item the second'
+		second_item.list = list_
 		second_item.save()
+
+		saved_list = List.objects.first() #
+		self.assertEquals(saved_list, list_) #
 
 		saved_items = Item.objects.all()
 		self.assertEquals(saved_items.count(),2)
@@ -67,4 +75,7 @@ class ItemModelTest(TestCase):
 		second_saved_item = saved_items[1]
 
 		self.assertEquals(first_saved_item.text, 'The first (ever) list item')
+		self.assertEquals(first_saved_item.list, list_)	#
 		self.assertEquals(second_saved_item.text, 'Item the second')
+		self.assertEquals(second_saved_item.list, list_) #
+		
